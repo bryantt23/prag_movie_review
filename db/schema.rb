5c5a85d1940d949a74e4f03441663da8aa92bf9a
bryantt23 <bryantt23@gmail.com>
2015-04-05 00:42:28 -0700

    Add basic layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405063011) do
+ActiveRecord::Schema.define(version: 20150405073617) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(version: 20150405063011) do
     t.decimal  "total_gross"
     t.text     "description"
     t.date     "released_on"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "cast"
+    t.string   "director"
+    t.string   "duration"
+    t.string   "image_file_name"
   end
 
 end
