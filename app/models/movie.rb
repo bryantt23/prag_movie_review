d814a0294abe9d270b034e477fb111e0bdbf181e
bryantt23 <bryantt23@gmail.com>
2015-04-06 01:30:05 -0700

    Add favorite button and create favorite resource

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,9 @@
 class Movie < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :fans, through: :favorites, source: :user
+
+
   validates :title, :released_on, :duration, presence: true
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
@@ -9,7 +13,6 @@ class Movie < ActiveRecord::Base
   }
   RATINGS = %w[G PG PG-13 R NC-17]
   validates :rating, inclusion: { in: RATINGS }
-
 
   def flop?
     total_gross.blank? || total_gross < 50000000
