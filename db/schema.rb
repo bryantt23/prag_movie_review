edd3843f59e17357e0f646be1faddf3a826cbba7
bryantt23 <bryantt23@gmail.com>
2015-04-06 02:40:24 -0700

    Add username and slug to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406093542) do
+ActiveRecord::Schema.define(version: 20150406093935) do
 
   create_table "characterizations", force: :cascade do |t|
     t.integer  "movie_id"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 20150406093542) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.boolean  "admin",           default: false
+    t.string   "username"
+    t.string   "slug"
   end
 
 end
